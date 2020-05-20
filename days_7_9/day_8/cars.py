1661ce3e04f7dfd77a0a2d73a8dac1acb21dded2
steven <mcgrath.steven7@gmail.com>
2020-05-20 15:23:27 -0400

    Day 35 - Continue refactoring
    Changes cars.py to use list comprehension rather than for loops (or nested for loops)

--- days_7_9/day_8/cars.py
+++ days_7_9/day_8/cars.py
@@ -1,4 +1,4 @@
-cars = {
+CARS = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
     'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
     'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
@@ -7,45 +7,42 @@ cars = {
 }
 
 
-def get_all_jeeps(cars=cars):
+def get_all_jeeps(cars=None):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    for make, models in cars.items():
-        if make == 'Jeep':
-            return ', '.join(models)
-
+    if cars is None:
+        cars = CARS
+    return ', '.join([model for make, model in cars.items() if make == 'Jeep'][0])
     pass
 
 
-def get_first_model_each_manufacturer(cars=cars):
+def get_first_model_each_manufacturer(cars=None):
     """return a list of matching models (original ordering)"""
-    first_models = []
-    for make, models in cars.items():
-        first_models.append(models[0])
+    if cars is None:
+        cars = CARS
+    first_models = [model for make, models in cars.items() for model in models if model == models[0]]
     return first_models
     pass
 
 
-def get_all_matching_models(cars=cars, grep='trail'):
+def get_all_matching_models(cars=None, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    matching_models = []
-    for make, models in cars.items():
-        for model in models:
-            if grep.lower() in model.lower():
-                matching_models.append(model)
+    if cars is None:
+        cars = CARS
+    matching_models = [model for make, models in cars.items() for model in models if grep.lower() in model.lower()]
     return sorted(matching_models)
     pass
 
 
-def sort_car_models(cars=cars):
+def sort_car_models(cars=None):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    for make, models in cars.items():
-        cars[make] = sorted(models)
-    return cars
+
+    if cars is None:
+        cars = CARS
+    sorted_cars = [sorted(models) for make, models in cars.items()]
+    return sorted_cars
+
     pass
-
-
-print(sort_car_models())
