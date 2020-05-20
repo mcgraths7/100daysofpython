bd2d6e04a04c56197f98f7411e16e978128edca2
steven <mcgrath.steven7@gmail.com>
2020-05-20 16:28:19 -0400

    Day 35 - Continue refactoring
    Changed rockpaperscissroslizardspock.py to remove Throw class, as it was redundant. Replaced with a constant THROW_DEFINITIONS
    
    Now checks whether player input exists in either the keys or values to get current throw

--- days_13_15/day_15/rockpaperscissroslizardspock.py
+++ days_13_15/day_15/rockpaperscissroslizardspock.py
@@ -2,7 +2,23 @@ import random
 import math
 
 from player import Player
-from throw import Throw
+
+
+THROW_DEFINITIONS = {
+    'r': 'rock',
+    'p': 'paper',
+    'sc': 'scissors',
+    'l': 'lizard',
+    'sp': 'spock'
+}
+
+WINNING_THROWS = {
+    'rock': [{'scissors': 'crushes'}, {'lizard': 'crushes'}],
+    'paper': [{'rock': 'covers'}, {'spock': 'disproves'}],
+    'scissors': [{'paper': 'cut'}, {'lizard': 'decapitate'}],
+    'lizard': [{'paper': 'eats'}, {'spock': 'poisons'}],
+    'spock': [{'rock': 'vaporizes'}, {'scissors': 'smashes'}]
+}
 
 
 def print_header():
@@ -20,21 +36,31 @@ def get_number_of_rounds():
 
 
 def get_current_throw():
-    throw_name = input('Please choose your throw: [r]ock, [p]aper, [s]cissors, [l]izard, or [sp]ock: ')
-    if throw_name not in ['r', 'rock', 'p', 'paper', 's', 'scissors', 'l', 'lizard', 'sp', 'spock']:
-        raise ValueError('Invalid input, please try again.')
-    return throw_name
+    inp = input('Please choose your throw: [r]ock, [p]aper, [sc]issors, [l]izard, or [sp]ock: ')
+    if inp in THROW_DEFINITIONS.keys():
+        return THROW_DEFINITIONS.get(inp)
+    elif inp in THROW_DEFINITIONS.values():
+        return inp
 
 
-def get_winning_text(throw1: Throw, throw2: Throw):
-    winning_throws = throw1.get_winning_throws()
-    losing_throws = throw2.get_winning_throws()
-    win = [winning_throw[throw2.name] for winning_throw in winning_throws if throw2.name in winning_throw.keys()]
-    lose = [losing_throw[throw1.name] for losing_throw in losing_throws if throw1.name in losing_throw.keys()]
+def get_winning_text(player_throw: str, cpu_throw: str):
+    win = [winning_throw[cpu_throw] for winning_throw in WINNING_THROWS[player_throw] if cpu_throw in winning_throw.keys()]
+    lose = [losing_throw[player_throw] for losing_throw in WINNING_THROWS[cpu_throw] if player_throw in losing_throw.keys()]
     return win, lose
 
 
-def game_loop(player1: Player, player2: Player, num_rounds: int, possible_throws: list):
+def check_if_victory(player1, player2, rounds_to_win):
+    if player1.rounds_won == rounds_to_win:
+        print(f"You've won {rounds_to_win} rounds! You've won the match!")
+        return True
+    elif player2.rounds_won == rounds_to_win:
+        print(f"Your opponent has won {rounds_to_win} rounds. You've lost the match")
+        return True
+    else:
+        return False
+
+
+def game_loop(player1: Player, player2: Player, num_rounds: int):
     current_round = 1
     rounds_to_win = math.floor((num_rounds / 2) + 1)
     print(f"First to {rounds_to_win} wins!")
@@ -42,32 +68,24 @@ def game_loop(player1: Player, player2: Player, num_rounds: int, possible_throws
         player_throw = None
         print(f"Round {current_round}... Throw!\n")
         try:
-            player_input = get_current_throw()
-            if player_input == 'r' or player_input == 'rock':
-                player_throw = Throw(name='rock')
-            elif player_input == 'p' or player_input == 'paper':
-                player_throw = Throw(name='paper')
-            elif player_input == 's' or player_input == 'scissors':
-                player_throw = Throw(name='scissors')
-            elif player_input == 'l' or player_input == 'lizard':
-                player_throw = Throw(name='lizard')
-            elif player_input == 'sp' or player_input == 'spock':
-                player_throw = Throw(name='spock')
+            player_throw = get_current_throw()
         except ValueError as ve:
             print(ve)
             continue
-        cpu_throw = Throw(random.choice(possible_throws))
-        print(f"You chose {player_throw.name}")
-        print(f"Your opponent chose {cpu_throw.name}")
+        cpu_throw = random.choice(list(THROW_DEFINITIONS.values()))
+
+        print(f"You chose {player_throw}")
+        print(f"Your opponent chose {cpu_throw}")
 
         winning_text = get_winning_text(player_throw, cpu_throw)
+
         if winning_text[0] and not winning_text[1]:
-            print(f"Your {player_throw.name} {winning_text[0][0]} opponent's {cpu_throw.name}."
+            print(f"Your {player_throw} {winning_text[0][0]} opponent's {cpu_throw}."
                   f" You've won this round!")
             player1.rounds_won += 1
             current_round += 1
         elif winning_text[1] and not winning_text[0]:
-            print(f"Your opponent's {cpu_throw.name} {winning_text[1][0]} your {player_throw.name}. "
+            print(f"Your opponent's {cpu_throw} {winning_text[1][0]} your {player_throw}. "
                   f"Unfortunately you've lost this round!")
             player2.rounds_won += 1
             current_round += 1
@@ -78,27 +96,23 @@ def game_loop(player1: Player, player2: Player, num_rounds: int, possible_throws
             print("Something went wrong... Next round!")
             continue
 
-        if player1.rounds_won == rounds_to_win:
-            print(f"You've won {rounds_to_win} rounds! You've won the match!")
-            break
-        elif player2.rounds_won == rounds_to_win:
-            print(f"Your opponent has won {rounds_to_win} rounds. You've lost the match")
+        if check_if_victory(player1, player2, rounds_to_win):
             break
         else:
             print(f"The current score is {player1.rounds_won} to {player2.rounds_won}. On to round {current_round}")
+            continue
 
 
 def main():
     print_header()
     num_rounds = get_number_of_rounds()
-    possible_throws = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
     player_name = input("Please input your name: ")
     player1 = Player(name=player_name)
     player2 = Player(name='cpu')
 
     print(f"{player1.name} vs. {player2.name}. Best out of {num_rounds} rounds wins!")
-    game_loop(player1, player2, num_rounds, possible_throws)
+    game_loop(player1, player2, num_rounds)
 
 
 if __name__ == '__main__':
